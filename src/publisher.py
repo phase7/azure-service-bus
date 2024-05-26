3d42692c65d2da6ecf5b62bb054ff44153f06acb
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-26 08:35:58 +0000

    refactor: Update publisher.py to use typer for CLI functionality

--- src/publisher.py
+++ src/publisher.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+import typer
 
 from auth import connection_string, topic_name
 from abstractions import ServiceBusPublisher, ServiceBusClientFactory, TopicMessageSenderStrategy
@@ -10,12 +11,17 @@ logger.setLevel(logging.DEBUG)
 logging.getLogger('azure.servicebus').setLevel(logging.WARNING)
 
 
-
 client = ServiceBusClientFactory.get_client(connection_string())
-pulisher = ServiceBusPublisher(connection_string=connection_string(),
-                               name=topic_name(),
-                               strategy=TopicMessageSenderStrategy)
+publisher = ServiceBusPublisher(connection_string=connection_string(),
+                                name=topic_name(),
+                                strategy=TopicMessageSenderStrategy)
+
+
+def publish_from_cli(message: str):
+    publisher.send_message(message_content=message)
 
 
 if __name__ == "__main__":
-    pulisher.send_message(message_content=f"Hello, World! it's {datetime.now()}")
+    publisher.send_message(
+        message_content=f"Hello, World! it's {datetime.now()}")
+    typer.run(publish_from_cli)
