46fa95025c0fe007a78b687fe9fa822c51a14086
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-26 14:45:18 +0600

    topic-subscriber implemeted (#3)
    * added pub sub files
    
    * added env example
    
    * testing dotenv
    
    * added abstractions
    to use service bus
    
    * refactor: update logger.info to use string formatting
    
    * chore: Update publisher.py to use ServiceBusPublisher for sending messages
    
    * refactor: Initialize strategy objects in ServiceBusPublisher and ServiceBusSubscriber
    
    * chore: Add functions for retrieving topic name and namespace name from environment variables
    
    * refactor: Update publisher.py to use typer for CLI functionality
    
    * chore: Add subscription name retrieval function and update related files
    
    * structure : Init project pubsub (#2)
    
    * added pub sub files
    
    * added env example
    
    * testing dotenv
    
    * added abstractions
    to use service bus

--- src/auth.py
+++ src/auth.py
@@ -5,3 +5,12 @@ load_dotenv()
 
 def connection_string():
     return os.getenv('SB_CONN_STRING')
+
+def topic_name():
+    return os.getenv('SB_TOPIC_NAME')
+
+def namespace_name():
+    return os.getenv('SB_NAMESPACE')
+
+def subscription_name():
+    return os.getenv('SB_SUBSCRIPTION_NAME')
--- src/publisher.py
+++ src/publisher.py
@@ -1,12 +1,27 @@
 import logging
+from datetime import datetime
+import typer
 
-from auth import connection_string
+from auth import connection_string, topic_name
+from abstractions import ServiceBusPublisher, ServiceBusClientFactory, TopicMessageSenderStrategy
 
 logger = logging.getLogger("publisher")
-# stop azure.service bus logging to console
+logger.setLevel(logging.DEBUG)
+# # stop azure.service bus logging to console
 logging.getLogger('azure.servicebus').setLevel(logging.WARNING)
 
-logger.setLevel(logging.DEBUG)
 
-# print("Publisher started", connection_string())
-logger.info(f"Connection string: {connection_string()}")
+client = ServiceBusClientFactory.get_client(connection_string())
+publisher = ServiceBusPublisher(connection_string=connection_string(),
+                                name=topic_name(),
+                                strategy=TopicMessageSenderStrategy)
+
+
+def publish_from_cli(message: str):
+    publisher.send_message(message_content=message)
+
+
+if __name__ == "__main__":
+    publisher.send_message(
+        message_content=f"Hello, World! it's {datetime.now()}")
+    typer.run(publish_from_cli)
