e29b5734f609c0adf0cb6465990ff116c62b1f31
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-26 07:20:48 +0000

    chore: Update publisher.py to use ServiceBusPublisher for sending messages

--- src/publisher.py
+++ src/publisher.py
@@ -1,13 +1,21 @@
 import logging
+from datetime import datetime
 
-from auth import connection_string
+from auth import connection_string, topic_name
+from abstractions import ServiceBusPublisher, ServiceBusClientFactory, TopicMessageSenderStrategy
 
 logger = logging.getLogger("publisher")
-# stop azure.service bus logging to console
+logger.setLevel(logging.DEBUG)
+# # stop azure.service bus logging to console
 logging.getLogger('azure.servicebus').setLevel(logging.WARNING)
 
-logger.setLevel(logging.DEBUG)
 
-# print("Publisher started", connection_string())
-logger.info("Connection string: %s", connection_string())
 
+client = ServiceBusClientFactory.get_client(connection_string())
+pulisher = ServiceBusPublisher(connection_string=connection_string(),
+                               name=topic_name(),
+                               strategy=TopicMessageSenderStrategy)
+
+
+if __name__ == "__main__":
+    pulisher.send_message(message_content=f"Hello, World! it's {datetime.now()}")
