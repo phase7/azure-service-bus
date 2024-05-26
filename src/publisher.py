13702faab79879b369785a341b7e09aa77c9a94e
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-26 03:49:05 +0000

    testing dotenv

---
+++ src/publisher.py
@@ -0,0 +1,12 @@
+import logging
+
+from auth import connection_string
+
+logger = logging.getLogger("publisher")
+# stop azure.service bus logging to console
+logging.getLogger('azure.servicebus').setLevel(logging.WARNING)
+
+logger.setLevel(logging.DEBUG)
+
+# print("Publisher started", connection_string())
+logger.info(f"Connection string: {connection_string()}")
