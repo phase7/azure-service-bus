84734b37abbd58e600904bb8e9b9eefcccb742b9
Ratul <89194947+ratul-cefalo@users.noreply.github.com>
2024-05-26 07:21:51 +0000

    chore: Add functions for retrieving topic name and namespace name from environment variables

--- src/auth.py
+++ src/auth.py
@@ -5,3 +5,9 @@ load_dotenv()
 
 def connection_string():
     return os.getenv('SB_CONN_STRING')
+
+def topic_name():
+    return os.getenv('SB_TOPIC_NAME')
+
+def namespace_name():
+    return os.getenv('SB_NAMESPACE')
